3c0e62707d3f8ab4a74da5e4c96a05e707db5182
Guillermo Cique Fernández <GuiyeC@gmail.com>
2022-11-15 15:40:38 +0100

    Helper SampleInput initializers

--- Sources/MapleDiffusion/Model/SampleInput.swift
+++ Sources/MapleDiffusion/Model/SampleInput.swift
@@ -51,3 +51,52 @@ public struct SampleInput {
         self.guidanceScale = guidanceScale
     }
 }
+
+#if os(iOS)
+import UIKit
+
+public extension SampleInput {
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: UIImage,
+        strength: Float = 0.75,
+        seed: Int = Int.random(in: 0...Int.max),
+        steps: Int = 50,
+        guidanceScale: Float = 5.0
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        self.initImage = initImage.cgImage!
+        self.strength = strength
+        self.seed = seed
+        self.steps = steps
+        self.guidanceScale = guidanceScale
+    }
+}
+#endif
+
+#if os(macOS)
+import AppKit
+
+public extension SampleInput {
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: NSImage,
+        strength: Float = 0.75,
+        seed: Int = Int.random(in: 0...Int.max),
+        steps: Int = 50,
+        guidanceScale: Float = 5.0
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        var imageRect = CGRect(x: 0, y: 0, width: initImage.size.width, height: initImage.size.height)
+        self.initImage = initImage.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)!
+        self.strength = strength
+        self.seed = seed
+        self.steps = steps
+        self.guidanceScale = guidanceScale
+    }
+}
+#endif
